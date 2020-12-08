df820b5306192cada4f5e324567f8bfe00fd5746
Richard <evildea@gmail.com>
2020-12-08 14:02:12 +1100

    Bug fix: Fixed

--- GameJamDuolingo/Source/GameJamDuolingo/GameJamDuolingoHUD.cpp
+++ GameJamDuolingo/Source/GameJamDuolingo/GameJamDuolingoHUD.cpp
@@ -10,7 +10,7 @@
 AGameJamDuolingoHUD::AGameJamDuolingoHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Assets/T/FirstPersonCrosshair.FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
